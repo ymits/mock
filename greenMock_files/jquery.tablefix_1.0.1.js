12c9492882da9226becd6d8a6c472480f06a10c6
mitsui <mitsui.yasuyuki@simplex-cn.co.jp>
2013-12-03 19:49:26 +0900

    commit

--- greenMock_files/jquery.tablefix_1.0.1.js
+++ greenMock_files/jquery.tablefix_1.0.1.js
@@ -5,56 +5,58 @@
  * http://www.otchy.net
  */
 (function($){
-	$.fn.tableScroll = function(options) {
-		return this.each(function(index){
+$.fn.tableScroll = function(options) {
+return this.each(function(index){
 
-			// 処理継続の判定
-			var opts = $.extend({}, options);
-			var baseTable = $(this);
-			opts.height = opts.height || baseTable.height();
+// 処理継続の判定
+var opts = $.extend({}, options);
+var baseTable = $(this);
+opts.height = opts.height || baseTable.height();
 
-			// 外部 div の設定
-			baseTable.wrap("<div></div>");
-			var div = baseTable.parent();
-			div.css({position: "relative"}).width(baseTable.outerWidth());
-			// スクロール部オフセットの取得
-			var offsetHY = baseTable.find('tbody tr:first-child').position().top || 0;
-			var offsetFY = baseTable.height();
-			baseTable.find('tfoot tr:first-child').each(function(){
-				offsetFY = $(this).position().top - 2;
-			});
-			
-			// テーブルの分割と初期化
-			var headTable = baseTable.wrap('<div></div>');
-			var footTable = baseTable.clone().wrap('<div></div>');
-			var bodyTable = baseTable.clone().wrap('<div></div>');
-			var headDiv = headTable.parent().css({position: "absolute", overflow: "hidden", width:"100%"});
-			var footDiv = footTable.parent().css({position: "absolute", overflow: "hidden", width:"100%"});
-			var bodyDivIn = bodyTable.parent().css({overflow: "hidden"}).wrap('<div></div>');
-			var bodyDivOut = bodyDivIn.parent().css({position: "absolute", overflow: "auto", width:"100%", position:"relative"});
-			div.append(footDiv).append(bodyDivOut);
+// スクロール部オフセットの取得
+//var offsetHY = baseTable.find('tbody tr:first-child').position().top || 0;
+var offsetHY = baseTable.find('tbody tr:first-child').offset().top - baseTable.offset().top;
+var offsetFY = baseTable.height();
+baseTable.find('tfoot tr:first-child').each(function(){
+offsetFY = $(this).offset().top - $(this).closest('table').offset().top;
+});
 
-			// 領域の設定
-			div.height(opts.height);
+// 外部 div の設定
+var baseWidth = baseTable.outerWidth();
+baseTable.wrap("<div></div>");
+var div = baseTable.parent();
+div.css({position: "relative"}).css({width:baseWidth+"px"});
+// テーブルの分割と初期化
+var headTable = baseTable.wrap('<div></div>').css({width:"100%"});
+var footTable = baseTable.clone().wrap('<div></div>').css({width:"100%"});
+var bodyTable = baseTable.clone().wrap('<div></div>').css({width:"100%"});
+var headDiv = headTable.parent().css({position: "absolute", overflow: "hidden", width:"100%"});
+var footDiv = footTable.parent().css({position: "absolute", overflow: "hidden", width:"100%"});
+var bodyDivIn = bodyTable.parent().css({overflow: "hidden"}).wrap('<div></div>');
+var bodyDivOut = bodyDivIn.parent().css({position: "absolute", overflow: "auto", width:"100%"});
+div.append(footDiv).append(bodyDivOut);
 
-			headDiv.height(offsetHY).css({top:'0px'});
-			headTable.height(offsetHY);
+// 領域の設定
+div.height(opts.height);
 
-			var footHeight = baseTable.height() - offsetFY + 2;
-			footDiv.height(footHeight).css({bottom:'0px'});
-			footTable.height(footHeight).css({
-				marginTop: -offsetFY + 'px'
-			});
+headDiv.height(offsetHY).css({top:'0px'});
+headTable.height(offsetHY);
 
-			var bodyHeight = opts.height - offsetHY - footHeight;
-			bodyDivOut.height(bodyHeight).css({top: offsetHY + 'px'});
-			bodyDivIn.height(baseTable.height() - offsetHY - footHeight);
-			bodyTable.height(bodyHeight).css({
-				marginTop: -offsetHY + 'px'
-			});
+var footHeight = baseTable.height() - offsetFY + 2;
+footDiv.height(footHeight).css({bottom:'0px'});
+footTable.height(footHeight).css({
+marginTop: -offsetFY + 'px'
+});
 
-			headTable.width(bodyTable.width());
-			footTable.width(bodyTable.width());
-		});
-	}
+var bodyHeight = opts.height - offsetHY - footHeight;
+bodyDivOut.height(bodyHeight).css({top: offsetHY + 'px'});
+bodyDivIn.height(baseTable.height() - offsetHY - footHeight);
+bodyTable.height(bodyHeight).css({
+marginTop: -offsetHY + 'px'
+});
+
+headTable.width(bodyTable.width());
+footTable.width(bodyTable.width());
+});
+}
 })(jQuery);
